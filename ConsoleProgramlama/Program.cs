51fef54bb74ac53e8d4eb2e495d43bd5a44b9182
Osman Melih ÖZASLAN <melihozaslan01@gmail.com>
2021-12-25 00:59:08 +0300

    Ödev Tamamlandı

---
+++ ConsoleProgramlama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleProgramlama
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("İsminizi Girin");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soy İsminizi Girin");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba" +" " + name + " " + surname);
+            
+
+        }
+    }
+}
